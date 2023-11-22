44f35a22e3ed7c1bada93911a5a3c13f622ef122
Nibu Krishna M.V <51090426+Nibzz007@users.noreply.github.com>
2023-11-22 23:46:25 +0530

    refresh icon

--- lib/view/account_view/account_screen.dart
+++ lib/view/account_view/account_screen.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:tinycc_app/network_service/network_api_services.dart';
 import 'package:tinycc_app/utils/constants/colors.dart';
 import 'package:tinycc_app/utils/constants/sizes.dart';
 import '../../model/account_model.dart';
 
-class AccountScreen extends StatelessWidget {
-  const AccountScreen({
+class AccountScreen extends StatefulWidget {
+  AccountScreen({
     super.key,
     required this.account,
+    this.auth
   });
 
   final TinyccAccountModel? account;
+  String? auth;
 
   @override
+  State<AccountScreen> createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen> {
+  int? updatedApiCallsCount;
+  @override
   Widget build(BuildContext context) {
-    final accountDetails = account?.account;
+    final accountDetails = widget.account?.account;
     return Scaffold(
       backgroundColor: AppColor.homeBackColor,
       appBar: AppBar(
         title: Text(
           'Hello, ${accountDetails?.username.toUpperCase()}',
         ),
+        actions: [
+          //! Not Working properly
+          IconButton(
+            onPressed: () async {
+               var updatedAccount = await NetworkApiServices().getAccountDetails(widget.auth);
+               setState(() {
+                 updatedApiCallsCount = updatedAccount!.account.counters.todayApiCalls.count;
+               });
+            },
+            icon: const Icon(Icons.refresh_sharp),
+          ),
+        ],
         centerTitle: true,
       ),
       body: Padding(
@@ -37,10 +58,11 @@ class AccountScreen extends StatelessWidget {
               ],
             ),
             AppSize.kHeight20,
-            Text('Version ${account?.version}'),
+            Text('Version ${widget.account?.version}'),
             AppSize.kHeight20,
             const Text('Daily API calls'),
-            Text('Count ${accountDetails.counters.todayApiCalls.count}'),
+
+            Text('Count ${updatedApiCallsCount ?? accountDetails.counters.todayApiCalls.count}'),
           ],
         ),
       ),
