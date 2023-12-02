f2b2e28bac9ed9ddca2b5fe917ec1e4cf359418a
Nibu Krishna M.V <51090426+Nibzz007@users.noreply.github.com>
2023-12-02 22:19:10 +0530

    changes in authorization view model

--- lib/view_model/authorization_view_model.dart/authorization_view_model.dart
+++ lib/view_model/authorization_view_model.dart/authorization_view_model.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthorizationViewModel extends GetxController {
-  TextEditingController authenticationHeaderController = TextEditingController();
+  TextEditingController authenticationHeaderController =
+      TextEditingController();
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  
-  void _setAuthDetail() async {
-    
+
+  Future<void> setAuthDetail(String key, String value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, value);
+    update();
+  }
+
+  Future<String?> getAuthDetails(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final authToken = prefs.getString(key);
+    return authToken;
   }
 }
---
+++ lib/view_model/splash_view_model/splash_view_model.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+import 'package:tinycc_app/view/home_view/home_screen.dart';
+import 'package:tinycc_app/view/login_view/login_screen.dart';
+import 'package:tinycc_app/view_model/authorization_view_model.dart/authorization_view_model.dart';
+
+class SplashViewModel extends GetxController {
+  AuthorizationViewModel controller = Get.put(AuthorizationViewModel());
+  void onInIt() {
+    super.onInit();
+    final token = controller.getAuthDetails('auth_token');
+    if(token == null) {
+      Get.offAll(LoginScreen());
+    } else {
+    }
+  }
+}
